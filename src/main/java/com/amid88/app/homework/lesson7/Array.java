0c4474a257d9e100f95e287844b220d494b7957a
amid88 <dmitriy.strashko@gmail.com>
2016-11-29 17:41:07 +0200

    HW7 - done

--- src/main/java/com/amid88/app/homework/lesson7/Array.java
+++ src/main/java/com/amid88/app/homework/lesson7/Array.java
@@ -124,4 +124,22 @@ public class Array {
         }
     }
 
+    public static void randomMatrixWithAlignment(){
+        int[][] matrix = new int[8][5];
+
+        //
+        for (int i = 0; i < matrix.length; i++ ){
+            for (int j = 0; j < matrix[0].length; j++ ){
+                matrix[i][j] = ThreadLocalRandom.current().nextInt(10, 999);
+            }
+        }
+
+        for (int i = 0; i < matrix.length; i++ ){
+            for (int j = 0; j < matrix[0].length; j++ ){
+                System.out. printf("%4d", matrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
 }
--- src/main/java/com/amid88/runners/LoopsArraysRunner.java
+++ src/main/java/com/amid88/runners/LoopsArraysRunner.java
@@ -2,17 +2,61 @@ package com.amid88.runners;
 
 import com.amid88.app.homework.lesson7.Array;
 
+import java.util.Scanner;
+
 /**
  * Created by dmitriy on 25.11.16.
  */
 public class LoopsArraysRunner {
 
     public static void main(String arg[]){
-        //Array.arrayPrintOutEvenNumbers();
-        //Array.arrayPrintOutOddNumbers();
-        //Array.calculateEvenOddNumbers();
-        //Array.calculateMinMaxOfArray();
-        Array.randomMatrix();
 
+        int parameterFlag = 1;
+        do {
+            int parameter = scannerInputAny("\nWelcome to program 'Lesson6'. Please choose one of options below: \n" + "\t"+
+                    "1. Print out even numbers\n" +"\t" +
+                    "2. Print out odd numbers\n" +"\t" +
+                    "3. Calculate even/odd numbers\n" + "\t" +
+                    "4. Calculate min and max value of array\n" + "\t" +
+                    "5. Random matrix\n" + "\t" +
+                    "6. Random matrix with alignment to the right\n" + "\t" +
+                    "0. Exit from the program");
+            switch (parameter) {
+                case 0: parameterFlag = 0;
+                    System.out.println("Thank you for using our program. Bye!");
+                    break;
+                case 1:
+                    Array.arrayPrintOutEvenNumbers();
+                    break;
+                case 2:
+                    Array.arrayPrintOutOddNumbers();
+                    break;
+                case 3:
+                    Array.calculateEvenOddNumbers();
+                    break;
+                case 4:
+                    Array.calculateMinMaxOfArray();
+                    break;
+                case 5:
+                    Array.randomMatrix();
+                    break;
+                case 6:
+                    Array.randomMatrixWithAlignment();
+                    break;
+                default:
+                    parameterFlag = 1;
+                    System.out.println("Wrong input");
+                    break;
+            }
+        } while (parameterFlag != 0);
+
+
+    }
+
+    public static int scannerInputAny (String s) {
+        System.out.println(s);
+        Scanner scannerS = new Scanner(System.in);
+        int scannerAny = Integer.parseInt(scannerS.nextLine());
+        return scannerAny;
     }
 }
