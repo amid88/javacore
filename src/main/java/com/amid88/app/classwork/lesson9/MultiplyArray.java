1023f5b6c49f5a487e5c9d2a542277f54c69ffb7
amid88 <dmitriy.strashko@gmail.com>
2016-12-06 19:21:55 +0200

    Classwork #9 2.12

---
+++ src/main/java/com/amid88/app/classwork/lesson9/MultiplyArray.java
@@ -0,0 +1,56 @@
+package com.amid88.app.classwork.lesson9;
+
+/**
+ * Created by dmitriy on 02.12.16.
+ */
+public class MultiplyArray {
+    public static void bubbleSortArrayAscending(){
+        int[] array = {3,5,1,10,4,7};
+        int tempInt;
+
+
+        for (int i = 0; i < array.length; i++){
+            for (int j = 0; j < array.length - 1; j++){
+                if (array[j] > array[j+1]) {
+                    tempInt = array[j+1];
+                    array[j+1] = array[j];
+                    array[j] = tempInt;
+                }
+            }
+
+        }
+
+
+        for (int i = 0; i < array.length; i++){
+            System.out.println(array[i]);
+
+        }
+
+
+    }
+
+    public static void bubbleSortArrayDescending() {
+        int[] array = {3, 5, 1, 10, 4, 7};
+        int tempInt;
+
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - 1; j++) {
+                if (array[j] < array[j + 1]) {
+                    tempInt = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = tempInt;
+                }
+            }
+
+        }
+
+        for (int i = 0; i < array.length; i++){
+            System.out.println(array[i]);
+
+        }
+
+    }
+
+
+}
