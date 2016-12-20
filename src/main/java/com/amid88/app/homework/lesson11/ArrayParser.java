d97dbfadc74eca1a28779c86fb064deb17c511f8
amid88 <dmitriy.strashko@gmail.com>
2016-12-20 17:57:03 +0200

    HW11_v0

---
+++ src/main/java/com/amid88/app/homework/lesson11/ArrayParser.java
@@ -0,0 +1,19 @@
+package com.amid88.app.homework.lesson11;
+
+/**
+ * Created by dstrashko on 12/20/16.
+ */
+public class ArrayParser {
+
+    private String inputString = "hello;world;once;again!";
+
+    public static String splitArray(String inputString){
+
+        return inputString.split(";").toString();
+
+
+
+
+
+    }
+}
---
+++ src/main/java/com/amid88/runners/homework/Lesson11Runner.java
@@ -0,0 +1,20 @@
+package com.amid88.runners.homework;
+
+import com.amid88.app.homework.lesson11.ArrayParser;
+
+/**
+ * Created by dstrashko on 12/20/16.
+ */
+public class Lesson11Runner {
+
+    public static void main(String[] args) {
+
+        String inputString = "hello;world;once;again!";
+
+        System.out.println(ArrayParser.splitArray(inputString));
+
+
+    }
+
+
+}
